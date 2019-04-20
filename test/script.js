5c42921e2f7296989495e96c2c683909b91468bb
Anton Mironov <ontonym@gmail.com>
2019-04-20 11:35:22 +0300

    fixed

--- test/script.js
+++ test/script.js
@@ -1,27 +1,30 @@
-var money = prompt('Какой ваш бюджет на месяц', '');
-
-var time = prompt('Ssrlbnt дату в формате YYYY-MM-DD', '');
-
-var appData = {};
-
-appData.budget = money;
-appData.timeData = time;
 
 
-var expenses = {};
-var optionalExpenses = {};
-
-var what = prompt('Введите обязательную статью расходов в этом месяце?', '');
-var cost = prompt('Во сколько это обойдется?', '');
-
-expenses.forWhat = what;
-expenses.costWat = cost;
+var money = prompt('Какой ваш бюджет', '');
+var date = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 
-console.log(expenses);
+var appData = {
+    budget: money,
+    timeData: date,
+    expenses: {
+        costOne: '',
+        reasonOne: '',
+    },
+    optionalExpenses: {
+        costTwo: '',
+        reasonTwo: '',
+    }
 
-var oneDay = (appData.budget - expenses.costWat)/30;
+};
 
-console.log(oneDay);
 
-alert('Ваш дневной бюджет не должен превышать '+ oneDay + ' рублей');
+appData.optionalExpenses.costOne = prompt('Цена 1', '');
+appData.optionalExpenses.reasonOne = prompt('Причина 1', '');
+appData.optionalExpenses.costTwo = prompt('Цена 2', '');
+appData.optionalExpenses.reasonTwo = prompt('Причина 2', '');
+
+var result = (parseInt(appData.optionalExpenses.costOne, 10) + 
+    parseInt(appData.optionalExpenses.costTwo, 10) + parseInt(appData.budget, 10))/30;
+
+alert('Ваш дневной бюджет = ' + result + 'руб');
